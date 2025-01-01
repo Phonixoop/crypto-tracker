c8f9667dc25cfc5bc6f0beb1429b75ff320c3dc7
Phonixoop <ali.hassanzadeh78@gmail.com>
2025-01-01 16:48:32 +0330

    remvoing env

--- src/env.js
+++ src/env.js
@@ -6,16 +6,7 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
-  server: {
-    AUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
-
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
-  },
+  server: {},
 
   /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
@@ -30,11 +21,7 @@ export const env = createEnv({
    * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
    * middlewares) or client-side so we need to destruct manually.
    */
-  runtimeEnv: {
-    AUTH_SECRET: process.env.AUTH_SECRET,
-
-    NODE_ENV: process.env.NODE_ENV,
-  },
+  runtimeEnv: {},
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
    * useful for Docker builds.
